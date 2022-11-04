5d1b1650699acaad23501f10ea8bdee55dbc1582
HeeyeonHeo <107686539+HeeyeonHeo@users.noreply.github.com>
2022-11-04 14:06:35 +0900

    2번째 업로드
    드롭 다운 메뉴, 배너 이미지, 인트로덕션 추가

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 // main.js
 
+$(function () {
+  // 배너 이미지 배열
+  let bgArr = [
+    "./img/main_banner_1.jpg",
+    "./img/main_banner_2.jpg",
+    "./img/main_banner_3.jpg",
+    "./img/main_banner_4.jpg",
+  ];
 
-console.log('main.js');
+  // 이미지 번호
+  let bg_img_num = 0;
+
+  // 배경 이미지 변경
+  // $("#main-banner .container").css({
+  //   "background-image": `url(${bgArr[bg_img_num]})`
+  // });
+
+  // 배경 재생
+  setInterval(function () {
+    bg_img_num++; // 이미지 번호 증가
+    if (bg_img_num >= bgArr.length) {
+      bg_img_num = 0;
+    }
+    console.log(bg_img_num);
+    $("#main-banner .container").css({
+      "background-image": `url(${bgArr[bg_img_num]})`,
+    });
+  }, 5000);
+});
